14686aa8dbfb414b3e28cfb0451533db5aca3ea9
Christianihechi <Christianihechi@gmail.com>
2023-08-16 00:11:54 +0100

    Update day_4_treasure map.py

--- day_4_treasure map.py
+++ day_4_treasure map.py
@@ -2,9 +2,9 @@ row1 = ['📦', '📦', '📦']
 row2 = ['📦', '📦', '📦']
 row3 = ['📦', '📦', '📦']
 treasure_map = [row1, row2, row3]
-# print("   a\t\tb\t\tc")
+
 print(f"{row1}\n{row2}\n{row3}")
-position = input("Hint ! Type just two digits. The first number is the 'column' and second number is the 'row'.\n"
+position = input("Hint ! Type just two digits. The first number is the 'column' and the second number is the 'row'.\n"
                  "Where do you want to put the treasure: ")
 
 target_row = int(position[1]) - 1  # Row
